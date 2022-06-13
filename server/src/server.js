85b21b6c5ccb3702acd7c760f3e56c6fa63e2fe6
Myrtleyerlte <dylan.rosevear143@gmail.com>
2022-06-12 23:57:14 -0500

    --

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const productsRouter = require('./routes/productFeedback.router');
+
 const server = http.createServer(app)
 const PORT = process.env.PORT || 7000
 
